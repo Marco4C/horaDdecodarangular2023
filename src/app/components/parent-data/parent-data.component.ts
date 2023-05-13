693c21f1c7fa99095dac43ad0e1d3453793a9689
Marco4C <marco4coutinho@gmail.com>
2023-05-13 00:57:35 -0300

    Aulas até a 8

---
+++ src/app/components/parent-data/parent-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-data',
+  templateUrl: './parent-data.component.html',
+  styleUrls: ['./parent-data.component.sass']
+})
+export class ParentDataComponent {
+  name='joãozinho'
+}
