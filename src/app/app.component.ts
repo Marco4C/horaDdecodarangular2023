a7ab813915e7403026bac8f34a5c1739c307ff7d
Marco4C <marco4coutinho@gmail.com>
2023-05-15 13:01:40 -0300

    ugaaa biiiii

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'horaDdecodarangular2023';
+  title:string = 'horaDecodarangular2023';
 }
--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-parent-data',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ParentDataComponent {
   name='joãozinho'
+  @Input() title: string = 'Title not found'
 }
