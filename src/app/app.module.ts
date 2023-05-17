467748e72ef95b1e674e81dbc7bea798c7167f71
Marco4C <marco4coutinho@gmail.com>
2023-05-17 18:08:19 -0300

    começa estilizar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,10 +8,12 @@ import { FirstComponentComponent } from './components/first-component/first-comp
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
-import { EventsComponent } from './components/events/events.component';
+import { EventsComponent } from './components/eventos/events.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
@@ -23,10 +26,13 @@ import { ListRenderComponent } from './components/list-render/list-render.compon
     EmitterComponent,
     ChangeNumberComponent,
     ListRenderComponent,
+    PipesComponent,
+    TwoWayBindingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.sass']
 })
 export class ListRenderComponent {
-  animals = [
-    {name: "Turca", type: "Dog"},
-    {name: "Tom", type: "Cat"},
-    {name: "Frida", type: "Dog"},
-    {name: "Bob", type: "Horse"},
+  animals: Animal[] = [
+    {name: "Turca", type: "Dog", age: 3},
+    {name: "Tom", type: "Cat", age: 4},
+    {name: "Frida", type: "Dog", age: 10},
+    {name: "Bob", type: "Horse", age: 1},
   ];
 
+  animal: Animal = {
+    name: "Happy",
+    type: "Dog",
+    age:  3
+  }
+  animalDetails = ''
+  showAge(animal: Animal) {
+    if (animal.age === 1) {
+      this.animalDetails = `O ${animal.type} ${animal.name} tem 1 ano!`;
+    } else {
+      this.animalDetails = `O ${animal.type} ${animal.name} tem ${animal.age} anos!`;
+    }
+  }
 }
